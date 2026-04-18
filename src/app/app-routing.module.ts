ddce5854b71359d1a41e88afc112810d2eddd994
Mohamed Saleh <mosalehtag74@icloud.com>
2026-04-19 01:41:09 +0200

    initial commit create component and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './features/students/pages/add-student/add-student.component';
+import { StudentsComponent } from './features/students/pages/students/students.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'add-student', component: AddStudentComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentsComponent } from './features/students/pages/students/students.component';
+import { StudentTableComponent } from './features/students/components/student-table/student-table.component';
+import { AddStudentComponent } from './features/students/pages/add-student/add-student.component';
+import { StudentDetailsComponent } from './features/students/pages/student-details/student-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    StudentsComponent,
+    StudentTableComponent,
+    AddStudentComponent,
+    StudentDetailsComponent
   ],
   imports: [
     BrowserModule,
